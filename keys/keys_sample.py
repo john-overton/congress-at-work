1647251b0c633979c88c977b3963f3d735d9808a
john-overton <john@jroverton.com>
2024-09-20 08:16:38 -0500

    added section for google gemini api key to sample key file

--- keys/keys_sample.py
+++ keys/keys_sample.py
@@ -7,3 +7,6 @@ consumer_secret = "consumersecrethere"
 bearer_token = "bearertokenhereblahblabalgbalhaldsfjgbn24oirgt90bhivnk43rpeojdf;lkd"
 oath2_userkey = "oath2userkeything"
 oath2_secret = "oath2secretthinggoeshereblahblahwo93gr0ejifmr"
+
+#Google Gemini Key
+gg_key = "googleapikeygoeshere234098tspoildfjl"
