d6159b2ca66b9fc2735b2f1499cad193376947a2
john-overton <john@jroverton.com>
2024-09-26 10:11:00 -0500

    added additional scripts to automate data collection

--- congress_api_scraper/run_daily_list.py
+++ congress_api_scraper/run_daily_list.py
@@ -5,5 +5,7 @@ SCRIPT_LIST = [
         "get_recent_active_bills.py",
         "get_active_bill_actions.py",
         "get_active_bills_urls.py",
-        "add_update_active_bill_text.py"
+        "add_update_active_bill_text.py",
+        "active_bill_data_collection_summary_local.py",
+        "active_bill_data_importance_collection_local.py",
         ]
