7e7d5940e610e0f7e05f29ad5b05a61bf21ce47d
john-overton <john@jroverton.com>
2024-09-29 13:27:08 -0500

    updated to include active legislation updates

--- x_bot/run_bot_list.py
+++ x_bot/run_bot_list.py
@@ -1,5 +1,6 @@
 # This is the list of POST features that used for @congressatwork
 
 SCRIPT_LIST = [
-        "didyouknow_post.py"
+        "didyouknow_post.py",
+        "active_bills_Post.py",
         ]
