46e8ea24443bc5676a20df1f36452184a5e042e2
john-overton <john@jroverton.com>
2024-09-30 08:37:14 -0500

    remvoed did you know again

--- x_bot/run_bot_list.py
+++ x_bot/run_bot_list.py
@@ -1,6 +1,5 @@
 # This is the list of POST features that used for @congressatwork
 
 SCRIPT_LIST = [
-        "didyouknow_post.py",
-        "active_bills_Post.py",
+        "active_bills_post.py",
         ]
